5414da39742045cf5c754ce48dfd7a78589cb3e8
Jeason <617394495@qq.com>
2015-09-11 16:06:33 +0800

    final

--- src/wechatauthdemo/ViewController/User/ADAboutViewController.m
+++ src/wechatauthdemo/ViewController/User/ADAboutViewController.m
@@ -9,7 +9,7 @@
 #import "ADAboutViewController.h"
 
 static NSString* const kTitleText = @"关于我们";
-static NSString* const kAboutUsText = @"微信登录Demo为微信团队开源项目，用于微信开发者进行微信登录、分享功能开发时的参考Demo。微信开发者可以参考项目中的代码来开发应用，也可以直接使用项目中的代码到自己的App中。\n开发者可以自由使用并传播本代码，但需要保留原作者信息。\n\n源代码下载地址：（git地址）\n联系我们：weixin-open@qq.com";
+static NSString* const kAboutUsText = @"微信登录Demo为微信团队开源项目，用于微信开发者进行微信登录、分享功能开发时的参考Demo。微信开发者可以参考项目中的代码来开发应用，也可以直接使用项目中的代码到自己的App中。\n开发者可以自由使用并传播本代码，但需要保留原作者信息。\n\n源代码下载地址：\n https://github.com/weixin-open/WeChatAuthDemo\n联系我们：weixin-open@qq.com";
 
 @interface ADAboutViewController ()
 
