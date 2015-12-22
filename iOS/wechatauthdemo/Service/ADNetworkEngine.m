ab6f0ef3fddd755a77b7e89d091f227d960d680c
Jeason <617394495@qq.com>
2015-12-22 11:55:41 +0800

    test

--- iOS/wechatauthdemo/Service/ADNetworkEngine.m
+++ iOS/wechatauthdemo/Service/ADNetworkEngine.m
@@ -14,7 +14,8 @@
 #import "ImageCache.h"
 #import "AES.h"
 
-static NSString* const defaultHost = @"http://demo.maizify.com";
+//static NSString* const defaultHost = @"http://demo.maizify.com";
+static NSString* const defaultHost = @"https://wechatauthdemo.com";
 static NSString* const publickeyFileName = @"rsa_public";
 
 @interface ADNetworkEngine ()
@@ -325,6 +326,9 @@ static NSString* const publickeyFileName = @"rsa_public";
     if (_manager == nil) {
         NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
         _manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:config];
+        AFJSONResponseSerializer *serializer = [AFJSONResponseSerializer serializerWithReadingOptions:NSJSONReadingAllowFragments];
+        serializer.acceptableContentTypes = [NSSet setWithObjects: @"application/json", @"text/html", @"text/plain", @"text/json", @"text/javascript", @"audio/wav", nil];
+        _manager.responseSerializer = serializer;
         _manager.sessionKey = [NSString randomKey];
         _manager.publicKey = self.RSAKey;
     }
